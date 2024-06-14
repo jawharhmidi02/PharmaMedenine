784722f907fc4260d6d282c592b62b5d6912df4d
Jawhar Hmidi <jawharhmidi02@gmail.com>
2024-06-14 02:59:21 +0100

    .

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('dotenv').config();
+
+const server = express();
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => 
+    console.log(`Server running on port ${PORT}`)
+);
